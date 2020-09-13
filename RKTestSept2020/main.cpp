5f16952c2817c93a7f82d5648295b934a61e9235
Adeeb Mohammed <adeeb2358@gmail.com>
2020-09-14 08:28:43 +0900

    find out pairs in an array of numbers

--- RKTestSept2020/main.cpp
+++ RKTestSept2020/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
+int a = 1000000000;
+
+int solution(std::vector<int> &A) {
+    std::unordered_map<int, int> pairMap;
+    int size = (int)A.size();
+  
+    // storing each number frequency of each number
+    for (int i = 0; i < size; i++) {
+        pairMap[A[i]]++;
+    }
+    
+    int pairCount = 0;
+    for (auto it = pairMap.begin(); it != pairMap.end(); ++it) {
+        int currCnt = it->second;
+        pairCount += (currCnt * (currCnt -1 )) /2;
+    }
+    
+    return pairCount;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<int> input = {3,5,6,3,3,5};
+    std::cout << solution(input);
+    std::cout << a;
     return 0;
 }

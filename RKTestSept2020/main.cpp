dae338656ad5af1e9f7794c22e463678944f5b09
Adeeb Mohammed <adeeb2358@gmail.com>
2020-09-13 22:19:19 +0900

    Initial Commit

---
+++ RKTestSept2020/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RKTestSept2020
+//
+//  Created by adeeb mohammed on 13/09/20.
+//  Copyright © 2020 adeeb mohammed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
